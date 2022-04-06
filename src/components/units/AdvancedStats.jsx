ec6b73e2da75f5d051ea4ffa6534c1be1bb21566
Edgar Aparicio Baeza <holamundo@edgarbaeza.mx>
2022-04-06 00:42:15 -0500

    Adding range column to advanced unit info

--- src/components/units/AdvancedStats.jsx
+++ src/components/units/AdvancedStats.jsx
@@ -9,7 +9,13 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Stack from '@mui/material/Stack';
 
-export const AdvancedStats = ({ unitname,  protoaction }) => {
+const mapValueDamage = {
+    'Ranged': 1,
+    'Hand': 2,
+    'Siege': 3
+}
+
+export const AdvancedStats = ({ unitname, protoaction }) => {
     return (
         <Box>
             <TableContainer>
@@ -19,25 +25,34 @@ export const AdvancedStats = ({ unitname,  protoaction }) => {
                             <TableCell>Name</TableCell>
                             <TableCell align="right">Damage</TableCell>
                             <TableCell align="right">ROF</TableCell>
+                            <TableCell align="right">Range</TableCell>
                             <TableCell align="right">Damage bonus</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {protoaction.filter(item => item?.name?.includes('Attack')).map((row) => (
-                            <TableRow key={row?.name}>
-                                <TableCell align='left'>{`${row.name} - ${row?.damagetype?.[0]}`}</TableCell>
-                                <TableCell align="right">{row?.damage && exactMath.round(row?.damage, -2)}</TableCell>
-                                <TableCell align="right">{row?.rof?.[0] && exactMath.round(row?.rof?.[0], -2)}</TableCell>
-                                <TableCell>{
-                                    row?.damagebonus && row.damagebonus.map(bonus => 
-                                        <Stack direction={'row'} spacing={1} key={`bonus-${bonus?._type}`}>
-                                            <Box>x{ exactMath.round(bonus?.__text, -2) }</Box>
-                                            <Box>{ bonus?._type?.replace('Abstract', '') }</Box>
-                                        </Stack>
-                                    )
-                                }</TableCell>
-                            </TableRow>
-                        ))}
+                        {protoaction
+                            .filter(item => item?.name?.includes('Attack'))
+                            .sort((a, b) => {
+                                const [damageTypeA] = a?.damagetype
+                                const [damageTypeB] = b?.damagetype
+                                return mapValueDamage[damageTypeA] - mapValueDamage[damageTypeB]
+                            })
+                            .map((row) => (
+                                <TableRow key={row?.name}>
+                                    <TableCell align='left'>{`${row.name} (${row?.damagetype?.[0]})`}</TableCell>
+                                    <TableCell align="right">{row?.damage && exactMath.round(row?.damage, -2)}</TableCell>
+                                    <TableCell align="right">{row?.rof?.[0] && exactMath.round(row?.rof?.[0], -2)}</TableCell>
+                                    <TableCell align="right">{row?.maxrange && exactMath.round(row?.maxrange, -2)}</TableCell>
+                                    <TableCell>{
+                                        row?.damagebonus && row.damagebonus.map(bonus =>
+                                            <Stack direction={'row'} spacing={1} key={`bonus-${bonus?._type}`}>
+                                                <Box>x{exactMath.round(bonus?.__text, -2)}</Box>
+                                                <Box>{bonus?._type?.replace('Abstract', '')}</Box>
+                                            </Stack>
+                                        )
+                                    }</TableCell>
+                                </TableRow>
+                            ))}
                     </TableBody>
                 </Table>
             </TableContainer>
