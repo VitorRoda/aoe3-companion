9fa2bbbe684735631a13253244fb2f27cec45ad9
Edgar Aparicio Baeza <holamundo@edgarbaeza.mx>
2022-04-11 00:26:47 -0500

    Compress assets

--- src/pages/UnitsInfo.jsx
+++ src/pages/UnitsInfo.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { getUnitsByCiv } from "../services/getUnitsByCiv";
 import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box'
 import { PanelUnit } from "../components/units/PanelUnit";
 
 export const UnitsInfo = ({ civ }) => {
@@ -16,12 +17,17 @@ export const UnitsInfo = ({ civ }) => {
   }, [civ])
 
   return (
-    <Grid container spacing={4}>
-      {units.map(unit =>
-        <Grid key={unit.id} item xs={12} sm={6} md={4} lg={3}>
-          <PanelUnit unit={unit} />
-        </Grid>
-      )}
-    </Grid>
+    !units?.length ?
+      <Box sx={{ py: 4, textAlign: 'center' }}>
+        <img width={300} src='/assets/revolution_guns.png' alt="Revolution guns" />
+      </Box> :
+      <Grid container spacing={4}>
+        {units.map(unit =>
+          <Grid key={unit.id} item xs={12} sm={6} md={4} lg={3}>
+            <PanelUnit unit={unit} />
+          </Grid>
+        )}
+      </Grid>
+
   )
 }
