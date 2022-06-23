b9d990b31532f40d47fdcf3a0bb299f708d607b9
Edgar Aparicio Baeza <holamundo@edgarbaeza.mx>
2022-06-23 15:49:29 -0500

    fix federal cards tab

--- src/components/MainDeck.jsx
+++ src/components/MainDeck.jsx
@@ -6,13 +6,13 @@ import Tab from '@mui/material/Tab';
 import TabsMui from '@mui/material/Tabs';
 import { translate } from '../utils/translator';
 import { CardList } from './cards/CardList';
+import classNames from 'classnames';
 
 export const MainDeck = React.memo(({ cards, showFederalCards, onClickCard }) => {
 	const [tabValue, setTabValue] = useState(0)
 
 	useEffect(() => {
 		setTabValue(0)
-		console.log(showFederalCards)
 	}, [showFederalCards])
 
 	const handleTabChange = useCallback((event, newValue) => {
@@ -43,11 +43,15 @@ export const MainDeck = React.memo(({ cards, showFederalCards, onClickCard }) =>
 	return (
 		<Box>
 			<Tabs value={tabValue} variant="scrollable" scrollButtons="auto" onChange={handleTabChange}>
-				<Tab label={<img width="20" src='/assets/Age0.png' alt='age0'></img>} value={0} />
+				<Tab label={
+					<img width="38" src='/assets/Age0.png' alt='age0' className={classNames({
+						'federal-cards-disabled': !showFederalCards
+					})}></img>
+				} value={4} disabled={!showFederalCards} />
+				<Tab label={translate('18966')} value={0} />
 				<Tab label={translate('19156')} value={1} />
 				<Tab label={translate('34352')} value={2} />
 				<Tab label={translate('18969')} value={3} />
-				<Tab label={translate('110265').replace(':', '')} value={4} disabled={!showFederalCards} />
 			</Tabs>
 
 			<Box className='deck-panel__container' hidden={tabValue !== 0}>
