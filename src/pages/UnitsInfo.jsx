bf6e7b5795e0798c0673b9b9f4f93cb013aca224
Edgar Aparicio Baeza <holamundo@edgarbaeza.mx>
2022-08-04 10:33:54 -0500

    Adding Input field for search units

--- src/pages/UnitsInfo.jsx
+++ src/pages/UnitsInfo.jsx
@@ -1,23 +1,28 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import Box from '@mui/material/Box'
+import { useTheme } from '@mui/material/styles'
+import TextField from '@mui/material/TextField'
 import { getAllUnitsByTypes } from '../services/proto.service';
 import { UnitTypeSelector } from '../components/units/UnitTypeSelector';
 import { DrawerUnit } from "../components/units/DrawerUnit";
 import { UnitList } from '../components/units/UnitList';
+import { translate } from '../utils/translator';
 
 export const UnitsInfo = () => {
   const PAGE_SIZE = 12
   const [types, seTypes] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
   const [units, setUnits] = useState([])
   const [paginatedUnits, setPaginatedUnits] = useState([])
   const [page, setPage] = useState(1)
   const [hasMore, setHasMore] = useState(false)
   const [selectedUnit, setSelectedUnit] = useState(null)
   const [openAdvInfo, setOpenAdvInfo] = useState(false)
+  const theme = useTheme()
 
   useEffect(() => {
     if (types.length) {
-      const data = getAllUnitsByTypes(types.map(type => type.id))
+      const data = getAllUnitsByTypes(types.map(type => type.id), searchTerm)
       setUnits(data)
       const paginated = data.slice(0, PAGE_SIZE)
       setPaginatedUnits(paginated)
@@ -31,6 +36,24 @@ export const UnitsInfo = () => {
   }, [types])
 
   useEffect(() => {
+    const timeoutID = setTimeout(() => {
+      if (types.length || searchTerm) {
+        const data = getAllUnitsByTypes(types.map(type => type.id), searchTerm)
+        setUnits(data)
+        const paginated = data.slice(0, PAGE_SIZE)
+        setPaginatedUnits(paginated)
+        setHasMore(paginated.length < data.length)
+      } else {
+        setUnits([])
+        setPaginatedUnits([])
+        setHasMore(false)
+      }
+    }, 400)
+
+    return () => clearTimeout(timeoutID)
+  }, [searchTerm])
+
+  useEffect(() => {
     if (page > 1) {
       const startAt = (page - 1) * PAGE_SIZE
       let limit = page * PAGE_SIZE
@@ -40,8 +63,12 @@ export const UnitsInfo = () => {
     }
   }, [page])
 
-  const handleOnChageTypes = useCallback((value) => {
+  const handleOnChangeTypes = useCallback((value) => {
     seTypes(() => value)
+  }, [])
+
+  const handleOnChangeSearch = useCallback((event) => {
+    setSearchTerm(event.target.value)
   }, [])
 
   const handleOnCloseAdvInfo = useCallback(() => {
@@ -61,8 +88,25 @@ export const UnitsInfo = () => {
 
   return (
     <Box>
-      <Box display={'flex'} justifyContent="center">
-        <UnitTypeSelector onChange={handleOnChageTypes} />
+      <Box display={'flex'} justifyContent="center" sx={{
+        [theme.breakpoints.down('sm')]: {
+          flexDirection: 'column',
+          alignItems: 'center'
+        }
+      }}>
+        <TextField
+          id="filled-search"
+          label={translate('21237')}
+          type="search"
+          value={searchTerm}
+          onChange={handleOnChangeSearch}
+          sx={{
+            mr: 1, [theme.breakpoints.down('sm')]: {
+              mr: 0, mb: 1
+            }
+          }}
+        />
+        <UnitTypeSelector onChange={handleOnChangeTypes} />
       </Box>
 
       {!units?.length &&
--- src/services/proto.service.js
+++ src/services/proto.service.js
@@ -1,5 +1,6 @@
 import { blacklistFlags, blackListUnitName, blackListUnitTypes } from '../constants'
 import protoData from '../data/protoy.json'
+import { translate } from '../utils/translator'
 
 export function getProtoInfo(id) {
     return protoData.unit.find(unit =>
@@ -14,18 +15,30 @@ const sortValue = {
     'AbstractOutlaw': 1
 }
 
-export function getAllUnitsByTypes(types = []) {
+export function getAllUnitsByTypes(types = [], searchTerm = '') {
     return protoData.unit.filter(unit => {
         const unitTypes = unit?.unittype || []
         const flags = unit?.flag || []
-
         if (!unitTypes) return false
 
-        return unitTypes.includes('Military')
+        const baseFilters = () => (
+            unitTypes.includes('Military')
             && !blackListUnitTypes.some(type => unitTypes.includes(type))
             && !blacklistFlags.some(flag => flags.includes(flag))
             && !blackListUnitName.some(namePart => unit._name.includes(namePart))
             && types.every(type => unitTypes.includes(type))
+        )
+
+        const normalizeStr = (str) => str.trim().toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+
+        const unitName = normalizeStr(translate(unit?.displaynameid))
+        const normalizedSearchTerm = normalizeStr(searchTerm)
+
+        if (searchTerm.trim()) {
+            return unitName.includes(normalizedSearchTerm) && baseFilters()
+        } else {
+            return baseFilters()
+        }
     }).sort((a, b) => getSortValue(a) - getSortValue(b))
 }
 
@@ -36,7 +49,7 @@ function getSortValue(unit) {
 
     for (let index = 0; index < keys.length; index++) {
         const key = keys[index];
-        
+
         if (unitTypes.includes(key)) {
             val = sortValue[key]
             break
