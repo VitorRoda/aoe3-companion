5dd2c6bd51d491031e0ea8cf1cf21c70e4de75b2
Edgar Aparicio Baeza <holamundo@edgarbaeza.mx>
2022-11-07 13:23:34 -0600

    Firebase hosting migration

--- src/utils/useGaTracker.js
+++ src/utils/useGaTracker.js
@@ -2,13 +2,15 @@ import { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import ReactGA from 'react-ga4'
 
+const GA_MEASUREMENT_ID = process.env.REACT_APP_GA_MEASUREMENT_ID
+
 export const useGaTracker = () => {
     const location = useLocation()
     const [initialized, setInitialized] = useState()
 
     useEffect(() => {
-        if (!window.location.href.includes('localhost')) {
-            ReactGA.initialize(process.env.REACT_APP_GA_MEASUREMENT_ID)
+        if (!window.location.href.includes('localhost') && GA_MEASUREMENT_ID) {
+            ReactGA.initialize(GA_MEASUREMENT_ID)
             setInitialized(true)
         }
     }, [])
